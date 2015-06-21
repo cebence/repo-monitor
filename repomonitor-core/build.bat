e9712fefd632cb062c3de9e75b1a395fb5d12d1a
cebence <dejan.cebetarevic@gmail.com>
2015-06-22 01:19:27 +0200

    Reformat code, improve "build.bat".

--- repomonitor-core/build.bat
+++ repomonitor-core/build.bat
@@ -3,9 +3,12 @@
 rem Restore NuGet packages before the build.
 nuget restore repomonitor-core.sln
 
-rem Set build parameters.
+rem Set configuration to debug, but allow the user to override it.
 set _CONFIG_=/p:Configuration=Debug
-set _PLATFORM_=/p:Platform=x86
+if not [%1]==[] set _CONFIG_=/p:Configuration=%1
+
+rem Set platform to the default one specified in the project.
+set _PLATFORM_=
 
 rem Use .NET Framework SDK 4.5 by default.
 set TARGET_SDK=
@@ -14,5 +17,3 @@ rem But if there's 4.5.1 use that one instead.
 if exist "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.1" set TARGET_SDK=/p:TargetFrameworkVersion=v4.5.1
 
 msbuild src\main\repomonitor-core.csproj %_CONFIG_% %_PLATFORM_% %TARGET_SDK%
-
-rem if %ERRORLEVEL%==0 echo Okay!
